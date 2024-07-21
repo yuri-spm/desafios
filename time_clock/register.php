036c5588093179e29212ca50778426ad0462df3c
Yuri do Monte <yspm.developer@gmail.com>
2024-07-20 21:15:17 -0300

    change register

--- time_clock/register.php
+++ time_clock/register.php
@@ -1,3 +1,29 @@
 <?php
+include_once "conect.php";
+include_once "helpers.php";
 
-echo $_POST;
+$action  =  $_GET['action'];
+
+date_default_timezone_set('America/Sao_Paulo');
+$dateNow =  date("H:i:s");
+$year = date('Y-m-d');
+
+$id = 1;
+
+switch ($action) {
+    case 'Registar Entrada':
+        entry_time($conn, $year, $dateNow, $id);
+        break;
+
+    case 'Registrar Pausa':
+        echo "Bem Almoço, pausa registada as {$dateNow}";
+
+
+        break;
+    case 'Registrar Retorno':
+        echo "Vamos Produzir, retorno as {$dateNow}";
+        break;
+    case 'Registrar Saída':
+        echo "Até amanhã, saída registrada as {$dateNow}";
+        break;
+}
