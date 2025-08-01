b189b4c20b9a4b75ceca12660042bd22f6c77a43
Yuri do Monte <yspm.developer@gmail.com>
2025-07-31 23:19:30 -0300

    config method unset

--- shopping_card/index.php
+++ shopping_card/index.php
@@ -1,70 +1,55 @@
 <?php
-
 session_start();
 // session_destroy();
 // echo "Sessão destruída. Atualize a página para continuar.";
 // exit;
-
 if (!isset($_SESSION["carrinho"]) || !is_array($_SESSION["carrinho"])) {
     $_SESSION["carrinho"] = [];
 }
-
 if (isset($_GET['remover'])) {
     $indice = (int) $_GET['remover'];
     if (isset($_SESSION["carrinho"][$indice])) {
-        unset($_SESSION["carrinho"]["$indice"]);
-
+        unset($_SESSION["carrinho"][$indice]);
         $_SESSION["carrinho"] = array_values($_SESSION["carrinho"]);
+
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
     }
 }
-
-
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $produto = [
         "nome"       => $_POST["nome"],
         "preco"      => (float) $_POST["preco"],
         "quantidade" => (int) $_POST["quantidade"]
     ];
-
     $_SESSION['carrinho'][] = $produto;
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
 }
 echo "<h2>Carrinho:</h2>";
 $totalGeral = 0;
-
 if (count($_SESSION["carrinho"]) === 0) {
     echo "Carrinho vazio.<br><br>";
 } else {
-    foreach ($_SESSION['carrinho'] as $item) {
+    foreach ($_SESSION['carrinho'] as $index => $item) {
         $total = $item['preco'] * $item["quantidade"];
         $totalGeral += $total;
-
         echo "Produto: {$item['nome']}<br>";
-        echo "Preço: {$item['preco']}<br>";
+        echo "Preço: R$ " .$item['preco']. "<br>";
         echo "Quantidade: {$item['quantidade']}<br>";
-        echo "Subtotal: R$ $total<br><br>";
+        echo "Subtotal: R$ " . $total . "<br><br>";
         echo "<a href='?remover=$index'>Remover este item</a><br><hr>";
     }
 }
-
-
-
-echo "<strong>Total geral: R$ $totalGeral</strong><br><br>";
-
-
-
-
+echo "<strong>Total geral: R$ " . number_format($totalGeral, 2, ',', '.') . "</strong><br><br>";
 ?>
 <!-- Formulário HTML -->
 <form method="POST">
     <label>Nome do produto:</label>
     <input type="text" name="nome" required><br><br>
-
     <label>Preço:</label>
     <input type="number" step="0.01" name="preco" required><br><br>
-
     <label>Quantidade:</label>
     <input type="number" name="quantidade" required><br><br>
-
     <button type="submit">Adicionar ao carrinho</button>
 </form>
